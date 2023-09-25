8d63194b2f5fcbd6e124b3e7eeca103f7fddd66f
TheK1ngArthur <artdiatei@alu.edu.gva.es>
2023-09-25 19:58:23 +0200

    [FINISH]Modificaciones actividad 227 y actividad 230 a 234 hechas

---
+++ php/src/234.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>234</title>
+</head>
+<body>
+<?php
+    $persones = [
+        ['nom' => 'Arthur','alçada' => 185],
+        ['nom' => 'Viti','alçada' => 200],
+        ['nom' => 'Boki','alçada' => 174],
+        ['nom' => 'Jadex','alçada' => 165],
+        ['nom' => 'NikoPiki','alçada' => 190]];
+    for ($i = 0; $i < count($persones)x; $i++) {
+        echo $persones[$i]['nom'];
+    }
+?>
+</body>
+</html>
