09192376eb2413aafd85851e3c80b17655a7cf4f
TheK1ngArthur <artdiatei@alu.edu.gva.es>
2023-09-25 22:23:32 +0200

    [FINISH/MOD]Cambios en la actividad 234 y finalizada actividad 235

--- php/src/235.php
+++ php/src/235.php
@@ -11,21 +11,23 @@
     <tr>
         <th>Nom</th>
         <th>Alçada</th>
+        <th>Email</th>
     </tr>
     </thead>
     <tbody>
     <?php
     $sumaAltura = 0;
     $persones = [
-        ['nom' => 'Arthur','alçada' => 185],
-        ['nom' => 'Viti','alçada' => 200],
-        ['nom' => 'Boki','alçada' => 174],
-        ['nom' => 'Jadex','alçada' => 170],
-        ['nom' => 'NikoPiki','alçada' => 190]];
+        ['nom' => 'Arthur','alçada' => 185,'email' => "arthurdias@gmail.com"],
+        ['nom' => 'Viti','alçada' => 200, 'email' => "viti@gmail.com"],
+        ['nom' => 'Boki','alçada' => 174, 'email' => "elboki@gmail.com"],
+        ['nom' => 'Jadex','alçada' => 170, 'email' => "jadex2004j@gmail.com"],
+        ['nom' => 'NikoPiki','alçada' => 190, 'email' => "nikopiki11@gmail.com"]];
     for ($i = 0; $i < count($persones); $i++) {
         echo '<tr>';
         echo '<td>' . $persones[$i]['nom'] . '</td>';
         echo '<td>' . $persones[$i]['alçada'] . '</td>';
+        echo '<td>' . $persones[$i]['email'] . '</td>';
         echo '<tr>';
         $sumaAltura += $persones[$i]['alçada'];
     }
@@ -33,6 +35,6 @@
     ?>
     </tbody>
 </table>
-    <div>Altura media : <?=$alturaMedia?></div>
+<div>Altura media : <?=$alturaMedia?></div>
 </body>
 </html>
